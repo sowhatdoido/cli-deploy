73c51acea3d0896ccf920339d65cab3b4fdd495e
Frank Tran <frank@freshiscool.com>
2016-03-07 22:39:32 -0800

    Add FTP and SFTP classes

---
+++ src/lib/FTPConnection.php
@@ -0,0 +1,74 @@
+<?php
+
+class FTPConnection implements ConnectionInterface {
+    private $_conn = null;
+    
+    public function __destruct(){
+        if(!empty($this->_conn))
+            ftp_close($this->_conn);
+        return;
+    }
+    
+    public function connect($server){
+        $this->_conn = ftp_connect($server);
+        
+        if($this->_conn)
+            return true;
+        return false;
+    }
+    
+    public function login($user, $pass){
+        if(ftp_login($this->_conn, $user, $pass)){
+            ftp_pasv($this->_conn, true);
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public function chdir($path){
+        return @ftp_chdir($this->_conn, $path);
+    }
+    
+    public function mkdir($path){
+        return ftp_mkdir($this->_conn, $path);
+    }
+    
+    public function rmdir($path){
+        return ftp_rmdir($this->_conn, $path);
+    }
+    
+    public function rename($old, $new){
+        return ftp_rename($this->_conn, $old, $new);
+    }
+    
+    public function getString($file){
+        ob_start();
+        $result = @ftp_get($this->_conn, "php://output", $file, FTP_BINARY);
+        $data = ob_get_contents();
+        ob_end_clean();
+        return $data;
+    }
+    
+    public function delete($file){
+        return ftp_delete($this->_conn, $file);
+    }
+    
+    public function rawlist($dir){
+        return ftp_nlist($this->_conn, $dir);
+    }
+    
+    public function isEmptyDir($dir){
+        $directorysize = $this->rawlist($dir);
+        return (sizeof($directorysize) == 2 && in_array('.', $directorysize) && in_array('..', $directorysize));
+    }
+    
+    public function put($path, $file){
+        return ftp_put($this->_conn, $file, $content, FTP_BINARY);
+    }
+    
+    public function putString($content, $file){
+        $stream = fopen('data://text/plain,' . $content,'r');
+        return ftp_fput($this->_conn, $file, $stream, FTP_BINARY);
+    }
+}
---
+++ src/lib/SFTPConnection.php
@@ -0,0 +1,62 @@
+<?php
+
+use phpseclib\Net\SFTP;
+
+class SFTPConnection implements ConnectionInterface {
+    private $_conn = null;
+    
+    public function connect($server){
+        //This method always return true on connect
+        $this->_conn = new SFTP($server);
+        return true;
+    }
+    
+    public function login($user, $pass){
+        if($this->_conn->login($user, $pass)){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public function chdir($path){
+        return $this->_conn->chdir($path);
+    }
+    
+    public function mkdir($path){
+        return $this->_conn->mkdir($path);
+    }
+    
+    public function rmdir($path){
+        return $this->_conn->rmdir($path);
+    }
+    
+    public function rename($old, $new){
+        return $this->_conn->rename($old, $new);
+    }
+    
+    public function getString($file){
+        return $this->_conn->get($file);
+    }
+    
+    public function delete($file){
+        return $this->_conn->delete($file);
+    }
+    
+    public function rawlist($dir){
+        return $this->_conn->rawlist($dir);
+    }
+    
+    public function isEmptyDir($dir){
+        $directorysize = $this->rawlist($dir);
+        return (sizeof($directorysize) == 2 && isset($directorysize['.']) && isset($directorysize['..']));
+    }
+    
+    public function put($path, $file){
+        return $this->_conn->put($file, $content, SFTP::SOURCE_LOCAL_FILE);
+    }
+    
+    public function putString($content, $file){
+        return $this->_conn->put($file, $content);
+    }
+}
