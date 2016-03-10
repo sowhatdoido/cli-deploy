f39f6ecb81ea7f1e0736d3c807c4ec403d738b01
Frank Tran <frank@freshiscool.com>
2016-03-09 23:06:23 -0800

    Missing string argument

--- src/lib/Console.php
+++ src/lib/Console.php
@@ -4,7 +4,7 @@ class Console {
     
     //Reminiscent and inspired by javascript's console.log command
     //Eventually add an optional file parameter
-    public static function log(){
+    public static function log($string){
         echo "{$string}" . PHP_EOL;
     }
     
