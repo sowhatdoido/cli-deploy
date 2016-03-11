cf39e0d566eacbda33420380eb35c5472214bf26
Frank Tran <frank@freshiscool.com>
2016-03-11 10:45:00 -0800

    Fix substring calculation in options formatting code block

--- src/app.php
+++ src/app.php
@@ -7,13 +7,12 @@
     //$_GET[0] main argument, increments for additional arguments
     parse_str(implode('&', array_slice($argv, 1)), $_GET['options']);
     foreach($_GET['options'] as $k => $v){
-        if($v === '' && substr($k, 1) !== '-'){
+        if($v === '' && substr($k, 0, 1) !== '-'){
             $_GET[] = $k;
             unset($_GET['options'][$k]);
         }
     }
-
-
+    
     //Check for config file
     if(!file_exists("{$_CWD}/deploy.config.php")){
         Console::log("deploy.config.php not found in {$_CWD}");
