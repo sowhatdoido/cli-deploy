f2205a4b4949a141f50a76b18041fbbc93892bb7
Frank Tran <frank@freshiscool.com>
2016-03-14 14:16:49 -0700

    Revert to http protocol for php versions < 5.6

--- src/commands/self-update.php
+++ src/commands/self-update.php
@@ -3,8 +3,10 @@
 use Humbug\SelfUpdate\Updater;
 
 $updater = new Updater(null, false);
-$updater->getStrategy()->setPharUrl('https://rawgit.com/sowhatdoido/pusheen/master/dist/pusheen.phar');
-$updater->getStrategy()->setVersionUrl('https://rawgit.com/sowhatdoido/pusheen/master/version');
+$http = (phpversion() >= "5.6")? "https:" : "http";
+
+$updater->getStrategy()->setPharUrl("{$http}//raw.githubusercontent.com/sowhatdoido/pusheen/master/dist/pusheen.phar");
+$updater->getStrategy()->setVersionUrl("{$http}//raw.githubusercontent.com/sowhatdoido/pusheen/master/version");
 try {
     $result = $updater->update();
     if ($result) {
